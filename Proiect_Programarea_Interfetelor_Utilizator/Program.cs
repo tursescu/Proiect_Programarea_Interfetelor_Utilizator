8197bf6a6936a7d1c434e6c54b936a622c8ef2e5
timo <tursescu@gmail.com>
2026-03-16 11:25:08 +0200

    Am finalizat cerintele din tema de laborator 3

--- Proiect_Programarea_Interfetelor_Utilizator/Program.cs
+++ Proiect_Programarea_Interfetelor_Utilizator/Program.cs
@@ -7,37 +7,94 @@ namespace Proiect_Programarea_Interfetelor_Utilizator
     class Program
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("1. Se creeaza produsele...");
-            Produs ecler = new Produs(1, "Ecler cu ciocolata", "Glazura de ciocolata", 15.5m);
-            Produs tarta = new Produs(2, "Tarta cu fructe", "Crema de mascarpone, fructe de padure, gelatina lamaie", 20.0m);
-            Console.WriteLine($"[Produs creat] {ecler.Nume} - {ecler.PretUnitar} lei");
-            Console.WriteLine($"[Produs creat] {tarta.Nume} - {tarta.PretUnitar} lei\n");
-
-            Console.WriteLine("2. Se creeaza comanda...");
-            DateTime dataLivrare = DateTime.Now.AddDays(1);
-            Comanda comandaMea = new Comanda(101, "Popescu", "Ion", "0722123456", dataLivrare);
-            Console.WriteLine($"[Comanda creata] ID: {comandaMea.ID} pentru {comandaMea.PrenumeClient} {comandaMea.NumeClient}");
-            Console.WriteLine($"[Status Initial] Comanda: {comandaMea.StatusComanda} | Plata: {comandaMea.StatusPlata}\n");
-
-            Console.WriteLine("3. Se adauga produse pe bon...");
-            comandaMea.AdaugaProdus(ecler, 2);
-            comandaMea.AdaugaProdus(tarta, 1);
-            foreach (ArticolComanda articol in comandaMea.Produse)
+        {
+            Produs[] vectorProduse = new Produs[100];
+            int numarProduse = 0;
+            bool ruleaza = true;
+            while (ruleaza)
             {
-                Console.WriteLine($"- Adaugat: {articol.Cantitate} buc. x {articol.ProdusComandat.Nume}");
-                Console.WriteLine($"  Pret: {articol.PretTotalArticol} lei");
+                Console.WriteLine("\n=== MENIU ===");
+                Console.WriteLine("1. Adauga produs (Citire + Salvare in vector)");
+                Console.WriteLine("2. Afiseaza produse (Afisare din vector)");
+                Console.WriteLine("3. Cauta produs (Cautare dupa nume)");
+                Console.WriteLine("0. Iesire");
+                Console.Write("Alege optiunea: ");
+
+                string optiune = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (optiune)
+                {
+                    case "1":
+                        CitireSiSalvareProdus(vectorProduse, ref numarProduse);
+                        break;
+                    case "2":
+                        AfisareVector(vectorProduse, numarProduse);
+                        break;
+                    case "3":
+                        CautareInVector(vectorProduse, numarProduse);
+                        break;
+                    case "0":
+                        ruleaza = false;
+                        break;
+                    default:
+                        Console.WriteLine("Optiune invalida!");
+                        break;
+                }
             }
-            Console.WriteLine();
+        }
+        static void CitireSiSalvareProdus(Produs[] vector, ref int contor)
+        {
+            if (contor >= 100)
+            {
+                Console.WriteLine("Vectorul este plin! Nu mai poti adauga produse.");
+                return;
+            }
+            Console.WriteLine("--- Adaugare Produs ---");
+            Console.Write("Nume: ");
+            string nume = Console.ReadLine();
+            Console.Write("Detalii: ");
+            string detalii = Console.ReadLine();
+            Console.Write("Pret unitar: ");
+            decimal pret = Convert.ToDecimal(Console.ReadLine());
+            Produs produsNou = new Produs(contor + 1, nume, detalii, pret);
+            vector[contor] = produsNou;
+            contor++;
+            Console.WriteLine("Produsul a fost salvat in vector!");
+        }
 
-            Console.WriteLine("4. Testare calcul Pret Total...");
-            Console.WriteLine($"Total de plata calculat automat: {comandaMea.PretTotal} lei\n");
+        static void AfisareVector(Produs[] vector, int contor)
+        {
+            Console.WriteLine("--- Produse in Vector ---");
+            if (contor == 0)
+            {
+                Console.WriteLine("Vectorul este gol.");
+                return;
+            }
+            for (int i = 0; i < contor; i++)
+            {
+                Console.WriteLine($"ID: {vector[i].ID} | Nume: {vector[i].Nume} | Pret: {vector[i].PretUnitar} lei | Detalii: {vector[i].Detalii}");
+            }
+        }
 
-            Console.WriteLine("5. Testare modificare statusuri...");
-            comandaMea.StatusComanda = StatusComanda.InProcesare;
-            comandaMea.StatusPlata = StatusPlata.Platita;
-            Console.WriteLine($"[Status Nou] Comanda: {comandaMea.StatusComanda} | Plata: {comandaMea.StatusPlata}\n");
-            Console.ReadLine();
+        static void CautareInVector(Produs[] vector, int contor)
+        {
+            Console.WriteLine("--- Cautare Produs ---");
+            Console.Write("Introdu cuvantul cautat in nume: ");
+            string cuvantCautat = Console.ReadLine().ToLower();
+            bool gasit = false;
+            for (int i = 0; i < contor; i++)
+            {
+                if (vector[i].Nume.ToLower().Contains(cuvantCautat))
+                {
+                    Console.WriteLine($"- Gasit pe pozitia {i} in vector: {vector[i].Nume} ({vector[i].PretUnitar} lei)");
+                    gasit = true;
+                }
+            }
+            if (gasit == false)
+            {
+                Console.WriteLine("Nu a fost gasit niciun produs care sa corespunda criteriului.");
+            }
         }
     }
 }
