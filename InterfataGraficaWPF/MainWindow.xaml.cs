df07b888874a739459bb52c44805541bbe7b91da
tursescu <tursescu@gmail.com>
2026-04-02 14:33:08 +0300

    Am introdus un proiect WPF si am facut afisarea listei de produse salvate in fisierul text.

---
+++ InterfataGraficaWPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using LibrarieModele.Models;
+using NivelStocareDate;
+using Proiect_Programarea_Interfetelor_Utilizator;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterfataGraficaWPF
+{
+    public partial class MainWindow : Window
+    {
+        private IStocareProduse adminProduse;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            adminProduse = StocareFactory.GetAdministratorStocareProduse();
+            IncarcaDateInTabel();
+        }
+
+        private void IncarcaDateInTabel()
+        {
+            var listaProduse = adminProduse.GetProduse();
+            tabelProduse.ItemsSource = listaProduse;
+        }
+
+        private void btnIncarca_Click(object sender, RoutedEventArgs e)
+        {
+            IncarcaDateInTabel();
+            MessageBox.Show("Datele au fost reîncărcate din fișier!", "Succes");
+        }
+    }
+}
---
+++ InterfataGraficaWPF/StocareFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using NivelStocareDate;
+
+namespace Proiect_Programarea_Interfetelor_Utilizator
+{
+    public static class StocareFactory
+    {
+        private const string FORMAT_SALVARE = "FormatSalvare";
+        private const string NUME_FISIER_PRODUSE = "NumeFisierProduse";
+        private const string NUME_FISIER_COMENZI = "NumeFisierComenzi";
+
+        public static IStocareProduse GetAdministratorStocareProduse()
+        {
+            string formatSalvare = ConfigurationManager.AppSettings[FORMAT_SALVARE] ?? "";
+            string numeFisier = ConfigurationManager.AppSettings[NUME_FISIER_PRODUSE] ?? "";
+
+            string locatieFisierSolutie = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName ?? "";
+            string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
+
+            if (formatSalvare != null)
+            {
+                switch (formatSalvare)
+                {
+                    case "txt":
+                        return new AdministrareProduseFisierText(caleCompletaFisier + "." + formatSalvare);
+                    case "memorie":
+                    default:
+                        return new AdministrareProduseMemorie();
+                }
+            }
+            return null;
+        }
+
+        public static IStocareComenzi GetAdministratorStocareComenzi()
+        {
+            string formatSalvare = ConfigurationManager.AppSettings[FORMAT_SALVARE] ?? "";
+            string numeFisier = ConfigurationManager.AppSettings[NUME_FISIER_COMENZI] ?? "";
+
+            string locatieFisierSolutie = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName ?? "";
+            string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
+
+            if (formatSalvare != null)
+            {
+                switch (formatSalvare)
+                {
+                    case "txt":
+                        return new AdministrareComenziFisierText(caleCompletaFisier + "." + formatSalvare);
+                    case "memorie":
+                    default:
+                        return new AdministrareComenziMemorie();
+                }
+            }
+            return null;
+        }
+    }
+}
